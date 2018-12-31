ef7b7a649efa8439c4d71f19becd4db7e42e8eff
x-pulse <40152105+x-pulse@users.noreply.github.com>
2018-12-31 18:04:41 +0300

    Публикация пинга.

---
+++ scrc/commands/ping.js
@@ -0,0 +1,11 @@
+jimp.read("https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/%D0%A1%D0%B5%D1%80%D1%8B%D0%B9_%D1%86%D0%B2%D0%B5%D1%82-_2014-03-15_18-16.jpg/220px-%D0%A1%D0%B5%D1%80%D1%8B%D0%B9_%D1%86%D0%B2%D0%B5%D1%82-_2014-03-15_18-16.jpg").then(function(b) {
+		jimp.loadFont("fonts/stc.fnt").then(function(fnt) {
+		b.print(fnt, 5,5, 'Ping is');
+		b.print(fnt, 5, 35, `${Date.now() - message.createdTimestamp}ms`);
+		b.print(fnt, 5,80, 'API Ping is');
+		b.print(fnt, 0,105, `${Math.round(client.ping)}ms`);
+		b.getBuffer(jimp.MIME_PNG, (error, buffer) => {
+				    message.channel.send({files: [{ name: 'file.png', attachment: buffer }] });
+				});
+		})
+})
